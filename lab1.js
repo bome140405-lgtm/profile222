044de02ae82efe091320e21902c79412fa6ed47c
Chien <bome140405@gmail.com>
2026-09-15 15:59:10 +0700

    Xong lab 1

---
+++ lab1.js
@@ -0,0 +1,10 @@
+console.log("day la lab1");
+// không dùng var
+let name="chien";
+const age =19;
+const diachi="ha noi";
+
+console.log("Tên Tôi Là:");
+console.log("Tuổi:",age);
+console.log("Địa Chỉ:",diachi);
+
