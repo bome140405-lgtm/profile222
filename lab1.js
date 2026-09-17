4e20e502239900e7329fca0be731567fa1aeaccd
Chien <bome140405@gmail.com>
2026-09-17 15:39:33 +0700

    lab1

--- lab1.js
+++ lab1.js
@@ -1,10 +1,27 @@
 console.log("day la lab1");
 // không dùng var
 let name="chien";
-const age =19;
+let age =19;
+age=21;
 const diachi="ha noi";
+const a =10;
+const b=5;
 
-console.log("Tên Tôi Là:");
+console.log("BÀI1");
+console.log("Tên Tôi Là:",name);
 console.log("Tuổi:",age);
 console.log("Địa Chỉ:",diachi);
+console.log("BÀI 2");
+console.log("Tổng",a+b);
+console.log("Hiệu",a-b);
+console.log("tích",a*b);
+console.log("Thương",a/b);
 
+console.log("BÀI3");
+function sayHello(name) {
+  console.log("Xin chào", name);
+  return 10;
+}
+sayHello("Dũng");
+sayHello("Mạnh");
+sayHello("Dương");
